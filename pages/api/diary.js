aa3b2655834472c62bf50f9a9ecb3e328d8facca
pongdang <pongdang0121@gmail.com>
2022-06-04 22:36:40 +0900

    feat: 서버사이드 캐시 추가

--- pages/api/diary.js
+++ pages/api/diary.js
@@ -1,4 +1,10 @@
 import { Octokit } from "@octokit/rest";
+import Cache from "lru-cache";
+
+const detailCache = new Cache({
+  // how long to live in ms
+  ttl: 1000 * 60 * 60 * 24 * 30,
+});
 
 const octokit = new Octokit({
   auth: process.env.GITHUB_TOKEN,
@@ -11,13 +17,19 @@ export default async function handler(req, res) {
     return res.status(500).json({ error: "path가 올바르지 않습니다." });
   }
 
+  if (detailCache.has(`/${path}`)) {
+    return res.status(200).json({ content: detailCache.get(`/${path}`) });
+  }
+
   const { data } = await octokit.rest.repos.getContent({
     owner: "pongdang",
     repo: "pongdang-diary-storage",
     path: `/${path}`,
   });
 
-  const { content } = data;
+  const content = Buffer.from(data.content, "base64").toString();
 
-  res.status(200).json({ content: Buffer.from(content, "base64").toString() });
+  detailCache.set(`/${path}`, content);
+
+  res.status(200).json({ content: content });
 }
--- pages/api/diarys.js
+++ pages/api/diarys.js
@@ -1,4 +1,10 @@
 import { Octokit } from "@octokit/rest";
+import Cache from "lru-cache";
+
+const listCache = new Cache({
+  // how long to live in ms
+  ttl: 1000 * 60 * 60 * 24 * 30,
+});
 
 const octokit = new Octokit({
   auth: process.env.GITHUB_TOKEN,
@@ -6,6 +12,11 @@ const octokit = new Octokit({
 
 export default async function handler(req, res) {
   const category = req.query.category;
+
+  if (listCache.has(`/${category}`)) {
+    return res.status(200).json({ list: listCache.get(`/${category}`) });
+  }
+
   const { data } = await octokit.rest.repos.getContent({
     owner: "pongdang",
     repo: "pongdang-diary-storage",
@@ -13,6 +24,8 @@ export default async function handler(req, res) {
   });
 
   const list = data.map((diary) => ({ name: diary.name, path: diary.path }));
+
+  listCache.set(`/${category}`, list);
 
   res.status(200).json({ list });
 }
