935285596d7a1dbf50bf754ae6af668a04acef53
pongdang <pongdang0121@gmail.com>
2022-06-04 22:26:36 +0900

    feat: backend 도 diary 통일

--- src/services/diary.js
+++ src/services/diary.js
@@ -1,9 +1,9 @@
 import { get } from "./api";
 
 export async function fetchDiaryList(category = "daily") {
-  return await get("/api/diary-list", { category });
+  return await get("/api/diarys", { category });
 }
 
 export async function fetchDiaryDetail(path) {
-  return await get("/api/diary-detail", { path });
+  return await get("/api/diary", { path });
 }
