809a655fc521c2768e32ecc12261076fda48c20b
pongdang <pongdang0121@gmail.com>
2022-06-10 18:58:47 +0900

    feat: 화면 크기에 따라 폰트 크기 조절

--- src/components/HelloDate.js
+++ src/components/HelloDate.js
@@ -10,6 +10,18 @@ export default function HelloDate() {
           margin: 0;
           font-size: 80px;
         }
+
+        @media (max-width: 422px) {
+          h2 {
+            font-size: 50px;
+          }
+        }
+
+        @media (max-width: 338px) {
+          h2 {
+            font-size: 30px;
+          }
+        }
       `}</style>
     </h2>
   );
