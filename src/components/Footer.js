63a16b3cdd32793ffdbca4b86e924c238e5cc5de
pongdang <pongdang0121@gmail.com>
2022-06-10 19:03:04 +0900

    feat: footer 컴포넌트!

---
+++ src/components/Footer.js
@@ -0,0 +1,30 @@
+const Pongdang = () => (
+  <span
+    style={{
+      color: "gray",
+      height: "1em",
+      marginLeft: "0.5rem",
+      outline: "none",
+    }}
+  >
+    <a href='https://github.com/pongdang' target='_blank' rel='noreferrer'>
+      {"@pongdang. "}
+    </a>
+  </span>
+);
+
+export default function Footer({ className }) {
+  return (
+    <footer
+      className={className}
+      style={{
+        padding: "1.9rem 0",
+        textAlign: "center",
+        margin: "0 auto",
+      }}
+    >
+      <Pongdang />
+      <span style={{ color: "#f5f5f5" }}>All rights reserved.</span>
+    </footer>
+  );
+}
