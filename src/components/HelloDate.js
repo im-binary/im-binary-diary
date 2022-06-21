190b1f06940910edef2b746c85136cecf5be9d94
pongdang <pongdang0121@gmail.com>
2022-06-21 23:11:33 +0900

    style: 필요없는 스타일 제거

--- src/components/HelloDate.js
+++ src/components/HelloDate.js
@@ -5,8 +5,6 @@ export default function HelloDate() {
       {DateYear}
       <style jsx>{`
         h2 {
-          -webkit-text-stroke-width: 1px;
-          -webkit-text-stroke-color: #f5f5f5;
           margin: 0;
           font-size: 80px;
         }
