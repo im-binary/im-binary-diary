ffa9d8ca46ee58807f50494c4c8ac4913a9e9882
pongdang <pongdang0121@gmail.com>
2022-06-23 22:19:45 +0900

    feat: 뒤로가기 버튼 스타일

--- src/components/GoBackButton.js
+++ src/components/GoBackButton.js
@@ -2,13 +2,22 @@ import Router from "next/router";
 
 export default function GoBackButton() {
   return (
-    <button onClick={() => Router.back()}>
+    <button className='backspace-button' onClick={() => Router.back()}>
       뒤로가기
       <style jsx>{`
         button {
+          border: none;
+          cursor: pointer;
+          padding: 8px 14px;
+          font-weight: bold;
           position: absolute;
           top: 10px;
           right: 12px;
+          border-radius: 10px;
+          transition: all 0.4s;
+        }
+        button:hover {
+          transform: scale(1.04);
         }
       `}</style>
     </button>
