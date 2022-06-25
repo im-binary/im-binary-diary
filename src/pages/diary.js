18c10d9c1169ae87de61d38cdd364bc08c7a47a2
pongdang <pongdang0121@gmail.com>
2022-06-25 11:53:02 +0900

    feat: 데이터 불러오기 전에 로딩 상태 보여주기

--- src/pages/diary.js
+++ src/pages/diary.js
@@ -6,14 +6,19 @@ import { fetchDiaryList } from "../services/diary";
 
 export default function DiaryList() {
   const [diaryList, setDiaryList] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     (async () => {
+      setLoading(true);
       const { data } = await fetchDiaryList();
       setDiaryList(data.list);
+      setLoading(false);
     })();
   }, []);
-  return (
+  return loading ? (
+    <h1>로딩</h1>
+  ) : (
     <>
       <Head>
         <link href='https://fonts.googleapis.com/css?family=Jua:400&display=optional' rel='stylesheet' />
@@ -30,46 +35,46 @@ export default function DiaryList() {
         </ul>
       </nav>
       <style jsx>{`
-        nav {
-          line-height: 1.9;
-          font-family: "Jua", sans-serif;
-        }
+      nav {
+        line-height: 1.9;
+        font-family: "Jua", sans-serif;
+      }
 
+      ul {
+        padding: 1rem 1.5rem;
+        gap: 1.3rem;
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+      }
+      
+      li {
+        border-radius: 10px;
+        transition: all 0.4s;
+      }
+
+      li:hover {
+        color: #4747ff;
+      }
+
+      a {
+        display: inline-block;
+        text-align: center;
+        width: 100%;
+        padding: 1.8rem 1.5rem;
+        font-size: 1.2rem;
+      }
+
+      @media (max-width: 800px) {
         ul {
-          padding: 1rem 1.5rem;
-          gap: 1.3rem;
-          display: grid;
-          grid-template-columns: 1fr 1fr 1fr;
+          grid-template-columns: 1fr 1fr;
         }
-        
-        li {
-          border-radius: 10px;
-          transition: all 0.4s;
+      }
+      
+      @media (max-width: 559px) {
+        ul {
+          grid-template-columns: 1fr;
         }
-
-        li:hover {
-          color: #4747ff;
-        }
-
-        a {
-          display: inline-block;
-          text-align: center;
-          width: 100%;
-          padding: 1.8rem 1.5rem;
-          font-size: 1.2rem;
-        }
-
-        @media (max-width: 800px) {
-          ul {
-            grid-template-columns: 1fr 1fr;
-          }
-        }
-        
-        @media (max-width: 559px) {
-          ul {
-            grid-template-columns: 1fr;
-          }
-      `}</style>
+    `}</style>
     </>
   );
 }
