a300c1485a11b94e218d9285392d308ebb1c55cb
pongdang <pongdang0121@gmail.com>
2022-07-09 16:00:34 +0900

    feat: Head 설정

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,14 @@ import "../styles/globals.css";
 import Layout from "../src/components/Layout";
 import { ThemeProvider } from "../context/themeProvider";
 import { GlobalStyle } from "../src/Theme/GlobalStyle";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Diary | All</title>
+      </Head>
       <ThemeProvider>
         <GlobalStyle />
         <Layout>
