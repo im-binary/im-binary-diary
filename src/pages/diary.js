1fa658592ceb0e90fad9bd54199de4c2702be27f
pongdang <pongdang0121@gmail.com>
2022-08-10 19:41:10 +0900

    feat: loading 중일 때 보여줄 화면

--- src/pages/diary.js
+++ src/pages/diary.js
@@ -16,8 +16,71 @@ export default function DiaryList() {
       setLoading(false);
     })();
   }, []);
+
   return loading ? (
-    <h1>로딩</h1>
+    <ul>
+      <li>
+        <a>로딩</a>
+      </li>
+      <li>
+        <a>로딩</a>
+      </li>
+      <li>
+        <a>로딩</a>
+      </li>
+      <style jsx>{`
+        ul {
+          padding: 1rem 1.5rem;
+          gap: 1.3rem;
+          display: grid;
+          grid-template-columns: 1fr 1fr 1fr;
+        }
+
+        li {
+          animation: skeleton-gradient 1.8s infinite ease-in-out;
+        }
+
+        li {
+          border-radius: 10px;
+          transition: all 0.4s;
+        }
+
+        a {
+          display: inline-block;
+          text-align: center;
+          width: 100%;
+          padding: 1.8rem 1.5rem;
+          font-size: 1.2rem;
+          opacity: 0;
+        }
+
+        @keyframes skeleton-gradient {
+          0% {
+            background-color: rgba(165, 165, 165, 0.1);
+          }
+
+          50% {
+            background-color: rgba(165, 165, 165, 0.3);
+          }
+
+          100% {
+            background-color: rgba(165, 165, 165, 0.1);
+          }
+        }
+
+        @media (max-width: 800px) {
+          ul {
+            grid-template-columns: 1fr 1fr;
+          }
+        }
+
+        @media (max-width: 559px) {
+          ul {
+            grid-template-columns: 1fr;
+          }
+        }
+      `}</style>
+    </ul>
   ) : (
     <>
       <Head>
@@ -35,46 +98,47 @@ export default function DiaryList() {
         </ul>
       </nav>
       <style jsx>{`
-      nav {
-        line-height: 1.9;
-        font-family: "Jua", sans-serif;
-      }
-
-      ul {
-        padding: 1rem 1.5rem;
-        gap: 1.3rem;
-        display: grid;
-        grid-template-columns: 1fr 1fr 1fr;
-      }
-      
-      li {
-        border-radius: 10px;
-        transition: all 0.4s;
-      }
-
-      li:hover {
-        color: #4747ff;
-      }
-
-      a {
-        display: inline-block;
-        text-align: center;
-        width: 100%;
-        padding: 1.8rem 1.5rem;
-        font-size: 1.2rem;
-      }
-
-      @media (max-width: 800px) {
-        ul {
-          grid-template-columns: 1fr 1fr;
+        nav {
+          line-height: 1.9;
+          font-family: "Jua", sans-serif;
         }
-      }
-      
-      @media (max-width: 559px) {
+
         ul {
-          grid-template-columns: 1fr;
+          padding: 1rem 1.5rem;
+          gap: 1.3rem;
+          display: grid;
+          grid-template-columns: 1fr 1fr 1fr;
         }
-    `}</style>
+
+        li {
+          border-radius: 10px;
+          transition: all 0.4s;
+        }
+
+        li:hover {
+          color: #4747ff;
+        }
+
+        a {
+          display: inline-block;
+          text-align: center;
+          width: 100%;
+          padding: 1.8rem 1.5rem;
+          font-size: 1.2rem;
+        }
+
+        @media (max-width: 800px) {
+          ul {
+            grid-template-columns: 1fr 1fr;
+          }
+        }
+
+        @media (max-width: 559px) {
+          ul {
+            grid-template-columns: 1fr;
+          }
+        }
+      `}</style>
     </>
   );
 }
