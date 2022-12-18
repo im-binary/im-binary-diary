8a989c3e81b41f535c6c6d1c5edb2b4a88f3fa33
pongdang <pongpong0121@kakao.com>
2022-12-18 13:29:46 +0900

    refactor: 스타일 컴포넌트 적용

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,29 +1,34 @@
-const Pongdang = () => (
-  <span
-    style={{
-      color: "gray",
-      marginRight: "4px",
-    }}
-  >
-    <a href='https://github.com/pongdang' target='_blank' rel='noreferrer'>
-      {"@pongdang."}
-    </a>
-  </span>
-);
+import styled, { css } from "styled-components";
 
 export default function Footer({ className }) {
   return (
-    <footer
-      className={className}
-      style={{
-        padding: "19px 0",
-        textAlign: "center",
-        margin: "0 auto",
-        fontSize: "1.4rem",
-      }}
-    >
+    <Container className={className}>
       <Pongdang />
-      <span className='footer-text'>All rights reserved.</span>
-    </footer>
+      <CC>All rights reserved.</CC>
+    </Container>
   );
 }
+
+const Pongdang = () => (
+  <Name css={css``}>
+    <a href='https://github.com/pongdang' target='_blank' rel='noreferrer'>
+      {"@pongdang."}
+    </a>
+  </Name>
+);
+
+const Name = styled.span`
+  color: gray;
+  margin-right: 4px;
+`;
+
+const CC = styled.span`
+  color: ${({ theme }) => theme.color};
+`;
+
+const Container = styled.footer`
+  padding: 19px 0;
+  margin: 0 auto;
+  text-align: center;
+  font-size: 1.4rem;
+`;
