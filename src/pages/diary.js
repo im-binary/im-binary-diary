a2db9c67a00ca820607fdaecb847d9a018fbdc0c
pongdang <pongpong0121@kakao.com>
2022-12-18 12:43:38 +0900

    refactor: 로딩 때 보여줄 컴포넌트와 실제 보여줄 컴포넌트와 분리

--- src/pages/diary.js
+++ src/pages/diary.js
@@ -1,8 +1,7 @@
-/* eslint-disable @next/next/no-page-custom-font */
-import Head from "next/head";
 import Link from "next/link";
 import { Fragment, useEffect, useState } from "react";
 import { fetchDiaryList } from "../services/diary";
+import styled from "styled-components";
 
 export default function DiaryList() {
   const [diaryList, setDiaryList] = useState([]);
@@ -17,152 +16,112 @@ export default function DiaryList() {
     })();
   }, []);
 
-  return loading ? (
-    <>
-      <h1>2222</h1>
-      <ul>
-        <li>
-          <a>로딩</a>
-        </li>
-        <li>
-          <a>로딩</a>
-        </li>
-        <li>
-          <a>로딩</a>
-        </li>
-      </ul>
-      <style jsx>{`
-        h1 {
-          transition: all 0.4s;
-          animation: skeleton-gradient 1.8s infinite ease-in-out;
-          color: transparent;
-          font-size: 2rem;
-          display: inline-block;
-          border-radius: 10px;
-        }
+  if (loading) {
+    return <Skeleton />;
+  }
 
-        ul {
-          padding: 10px 15px;
-          gap: 13px;
-          display: grid;
-          grid-template-columns: 1fr 1fr 1fr;
-        }
-
-        li {
-          animation: skeleton-gradient 1.8s infinite ease-in-out;
-        }
-
-        li {
-          border-radius: 10px;
-          transition: all 0.4s;
-        }
-
-        a {
-          display: inline-block;
-          text-align: center;
-          width: 100%;
-          padding: 18px 15px;
-          font-size: 16px;
-          opacity: 0;
-        }
-
-        @keyframes skeleton-gradient {
-          0% {
-            background-color: rgba(165, 165, 165, 0.1);
-          }
-
-          50% {
-            background-color: rgba(165, 165, 165, 0.3);
-          }
-
-          100% {
-            background-color: rgba(165, 165, 165, 0.1);
-          }
-        }
-
-        @media (max-width: 800px) {
-          ul {
-            grid-template-columns: 1fr 1fr;
-          }
-        }
-
-        @media (max-width: 559px) {
-          ul {
-            grid-template-columns: 1fr;
-          }
-        }
-      `}</style>
-    </>
-  ) : (
-    <>
-      <nav>
-        {Object.keys(diaryList)
-          .sort((a, b) => b - a)
-          .map((year, index) => (
-            <Fragment key={`${year}-${index}`}>
-              <h1>{year}</h1>
-              <ul>
-                {diaryList[year].length === 0 ? (
-                  <li>없습니당</li>
-                ) : (
-                  diaryList[year].map((diary) => (
-                    <li className='diary-box' key={diary.path}>
-                      <Link href={`/${diary.path}`}>
-                        <a>{diary.name}</a>
-                      </Link>
-                    </li>
-                  ))
-                )}
-              </ul>
-            </Fragment>
-          ))}
-      </nav>
-      <style jsx>{`
-        nav {
-          line-height: 1.9;
-          font-family: "Jua", sans-serif;
-        }
-
-        h1 {
-          font-size: 2rem;
-        }
-
-        ul {
-          padding: 10px 15px;
-          gap: 13px;
-          display: grid;
-          grid-template-columns: 1fr 1fr 1fr;
-        }
-
-        li {
-          border-radius: 10px;
-          transition: all 0.4s;
-        }
-
-        li:hover {
-          color: #4747ff;
-        }
-
-        a {
-          display: inline-block;
-          text-align: center;
-          width: 100%;
-          padding: 18px 15px;
-          font-size: 16px;
-        }
-
-        @media (max-width: 800px) {
-          ul {
-            grid-template-columns: 1fr 1fr;
-          }
-        }
-
-        @media (max-width: 559px) {
-          ul {
-            grid-template-columns: 1fr;
-          }
-        }
-      `}</style>
-    </>
+  return (
+    <Nav>
+      {Object.keys(diaryList)
+        .sort((a, b) => b - a)
+        .map((year, index) => (
+          <Fragment key={`${year}-${index}`}>
+            <DiaryYear>{year}</DiaryYear>
+            <Ul>
+              {diaryList[year].length === 0 ? (
+                <li>없습니당</li>
+              ) : (
+                diaryList[year].map((diary) => (
+                  <li className='diary-box' key={diary.path}>
+                    <Link href={`/${diary.path}`}>
+                      <a>{diary.name}</a>
+                    </Link>
+                  </li>
+                ))
+              )}
+            </Ul>
+          </Fragment>
+        ))}
+    </Nav>
   );
 }
+
+const Skeleton = () => {
+  return (
+    <Nav>
+      <DiaryYear className='skeleton'>2022</DiaryYear>
+      <Ul className='skeleton'>
+        <li>
+          <a>로딩</a>
+        </li>
+        <li>
+          <a>로딩</a>
+        </li>
+        <li>
+          <a>로딩</a>
+        </li>
+      </Ul>
+    </Nav>
+  );
+};
+
+const Nav = styled.nav`
+  line-height: 1.9;
+`;
+
+const DiaryYear = styled.h1`
+  font-size: 2rem;
+
+  &.skeleton {
+    transition: all 0.4s;
+    animation: skeleton-gradient 1.8s infinite ease-in-out;
+    color: transparent;
+    font-size: 2rem;
+    display: inline-block;
+    border-radius: 10px;
+  }
+`;
+
+const Ul = styled.ul`
+  padding: 10px 15px;
+  gap: 13px;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+
+  li {
+    border-radius: 10px;
+    transition: all 0.4s;
+  }
+
+  li:hover {
+    color: #4747ff;
+  }
+
+  a {
+    display: inline-block;
+    text-align: center;
+    width: 100%;
+    padding: 18px 15px;
+    font-size: 16px;
+  }
+
+  &.skeleton li {
+    animation: skeleton-gradient 1.8s infinite ease-in-out;
+  }
+
+  &.skeleton li a {
+    opacity: 0;
+  }
+
+  @media (max-width: 800px) {
+    ul {
+      grid-template-columns: 1fr 1fr;
+    }
+  }
+
+  @media (max-width: 559px) {
+    ul {
+      grid-template-columns: 1fr;
+    }
+  }
+`;
