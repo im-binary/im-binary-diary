7f641a97e3daf4c9fbd610ef26c27ca45ac557d4
pongdang <pongpong0121@kakao.com>
2022-12-18 13:15:14 +0900

    refactor: 중복되었던 이미지 관련 코드 수정

--- src/components/CloudAnimation.js
+++ src/components/CloudAnimation.js
@@ -1,4 +1,4 @@
-import { css } from "styled-components";
+import styled, { css } from "styled-components";
 import nextConfig from "next/config";
 
 const { publicRuntimeConfig } = nextConfig();
@@ -6,87 +6,55 @@ const { publicRuntimeConfig } = nextConfig();
 const ASSET_PREFIX = publicRuntimeConfig.ASSET_PREFIX;
 
 export default function CloudAnimation({ toggle, mode }) {
+  const modeImageSrc = (mode) => {
+    if (mode === "dark") {
+      return `${ASSET_PREFIX}/moon.png`;
+    } else if (mode === "light") {
+      return `${ASSET_PREFIX}/sun.png`;
+    }
+  };
+
   return (
-    <div className='container' css={container}>
-      {mode === "dark" ? (
-        <>
+    <Container>
+      <button onClick={toggle}>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img className='mode-img' src={modeImageSrc(mode)} alt={mode} />
+        <CloudImgContainer>
           {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img className='img-moon' src={`${ASSET_PREFIX}/moon.png`} alt='moon' />
-          <div className='cloud-container' onClick={toggle}>
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img className='img-cloud' src={`${ASSET_PREFIX}/cloud.png`} alt='cloud' />
-          </div>
-        </>
-      ) : (
-        <>
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img className='img-sun' src={`${ASSET_PREFIX}/sun.png`} alt='sun' />
-          <div className='cloud-container' onClick={toggle}>
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img className='img-cloud' src={`${ASSET_PREFIX}/cloud.png`} alt='cloud' />
-          </div>
-        </>
-      )}
-      <style jsx>{`
-        .container {
-          width: 100%;
-          height: 135px;
-          text-align: center;
-          position: relative;
-          margin-bottom: 30px;
-        }
-
-        .img-moon,
-        .img-sun {
-          width: 100px;
-          margin: 0 auto;
-        }
-
-        .cloud-container {
-          position: absolute;
-          transform: translate(-50%, -50%);
-          top: 62%;
-          left: 50%;
-        }
-
-        .img-cloud {
-          width: 200px;
-          animation: flow 5s linear 0s infinite alternate;
-        }
-
-        @keyframes flow {
-          from {
-            transform: translate(-40%, 0);
-          }
-          to {
-            transform: translate(40%, 0);
-          }
-
-          0% {
-            opacity: 0.6;
-          }
-          25% {
-            opacity: 0.8;
-          }
-          50% {
-            opacity: 1;
-          }
-          75% {
-            opacity: 0.8;
-          }
-          100% {
-            opacity: 0.6;
-          }
-        }
-
-        @media (max-width: 422px) {
-          .img-cloud {
-            width: 150px;
-          }
-        }
-      `}</style>
-    </div>
+          <img src={`${ASSET_PREFIX}/cloud.png`} alt='cloud' />
+        </CloudImgContainer>
+      </button>
+    </Container>
   );
 }
 
-const container = css``;
+const Container = styled.div`
+  width: 100%;
+  height: 135px;
+  text-align: center;
+  position: relative;
+  margin-bottom: 30px;
+
+  .mode-img {
+    width: 100px;
+    margin: 0 auto;
+  }
+`;
+
+const CloudImgContainer = styled.div`
+  position: absolute;
+  transform: translate(-50%, -50%);
+  top: 62%;
+  left: 50%;
+
+  img {
+    width: 200px;
+    animation: flow 5s linear 0s infinite alternate;
+  }
+
+  @media (max-width: 422px) {
+    img {
+      width: 150px;
+    }
+  }
+`;
